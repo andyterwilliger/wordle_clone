ba1922b370a8188712030219c77dc6df9c2ae8a1
Andrew Terwilliger <aterwilliger@ATerwilliger-CHI-2021-MBP14.local>
2023-01-25 19:06:26 -0600

    refactored winning/losing code

--- app.js
+++ app.js
@@ -1,90 +1,129 @@
 //variables
-const letters = document.querySelectorAll('.letter');
+const letters = document.querySelectorAll(".letter");
 const answer_length = 5;
-const loadingDiv = document.querySelector('.loading-header')
-console.log(letters)
+const ROUNDS = 6;
+const loadingDiv = document.querySelector(".loading-header");
+console.log(letters);
 
-async function init(){
-    let currentGuess = "";
-    let currentRow = 0;
+async function init() {
+  let currentGuess = "";
+  let currentRow = 0;
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+  const resObj = await res.json();
+  //destructuring = const { word } = await res.json()
+  const word = resObj.word.toUpperCase();
+  const wordParts = word.split("");
+  const map = makeMap(wordParts);
+  let done = false;
+  console.log(map);
+  console.log(wordParts);
+  console.log(word);
 
-    const res = await fetch('https://words.dev-apis.com/word-of-the-day');
-    const resObj = await res.json();
-    //destructuring = const { word } = await res.json()
-    const word = resObj.word.toUpperCase();
-    console.log(word)
-
-    function addLetter(letter){
-        //setting answer length instead of 5 here to be clear in code
-        if(currentGuess.length < answer_length){
-            //add letter to the end 
-            currentGuess+=letter;
-        } else{
-            //replaces the last letter
-            currentGuess = currentGuess.substring(0, currentGuess.length-1) + letter;
-        }
-        
-        letters[answer_length * currentRow + currentGuess.length - 1].innerText = letter;
+  function addLetter(letter) {
+    //setting answer length instead of 5 here to be clear in code
+    if (currentGuess.length < answer_length) {
+      //add letter to the end
+      currentGuess += letter;
+    } else {
+      //replaces the last letter
+      currentGuess =
+        currentGuess.substring(0, currentGuess.length - 1) + letter;
     }
 
-    async function commit(){
-        if(currentGuess.length != answer_length){
-            //do nothing
-            return;
-        } else {
-            //TODOneed to validate word
+    letters[answer_length * currentRow + currentGuess.length - 1].innerText =
+      letter;
+  }
 
-            //TODO marking 'correct' 'wrong' 'close'
-            currentRow++;
-            currentGuess = '';
-        }
+  async function commit() {
+    if (currentGuess.length != answer_length) {
+      //do nothing
+      return;
+    } 
+      
+    //TODOneed to validate word
+    //TODO marking 'correct' 'wrong' 'close'
+    const guessParts = currentGuess.split("");
+
+    for (let i = 0; i < answer_length; i++) {
+      //mark as correct
+      if (guessParts[i] === wordParts[i]) {
+        //it's right
+        letters[currentRow * answer_length + i].classList.add("correct");
+        map[guessParts[i]]--;
+      }
     }
 
-    function backspace(){
-        currentGuess = currentGuess.substring(0, currentGuess.length - 1 )
-        letters[answer_length * currentRow + currentGuess.length].innerText = ''
+    for (let i = 0; i < answer_length; i++) {
+      if (guessParts[i] === wordParts[i]) {
+        //already done, do nothing
+      } else if (wordParts.includes(guessParts[i]) && map[guessParts[i]] > 0) {
+        letters[currentRow * answer_length + i].classList.add("close");
+        map[guessParts[i]]--;
+      } else {
+        letters[currentRow * answer_length + i].classList.add("incorrect");
+      }
     }
+
+    currentRow++;
+    currentGuess = "";
+
+    //lose
+    if (currentGuess === word) {
+      // YOU WIN
+      done = true;
+      alert("YOU WIN");
+      
+    } else if (currentRow === ROUNDS) {
     
-    function setLoading(isLoading){
-        console.log(isLoading)
-        loadingDiv.classList.toggle('show', !isLoading)
+      alert(`You lose! The word was ${word}`);
+      done = true;
     }
+    currentGuess=''
+  }
 
-    
-    document.addEventListener('keydown', function handleKeyPress(e){
-        const action = e.key;
-        console.log(action)
-
-        if(action === 'Enter'){
-            commit();
-        } else if(action === 'Backspace'){
-            backspace();
-        }
-         else if(isLetter(action)){
-            addLetter(action.toUpperCase())
-         } else{
-            alert('Not a valid letter!')
-         }
-    })
+function backspace() {
+  currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+  letters[answer_length * currentRow + currentGuess.length].innerText = "";
 }
 
+function setLoading(isLoading) {
+  console.log(isLoading);
+  loadingDiv.classList.toggle("show", !isLoading);
+}
 
+document.addEventListener("keydown", function handleKeyPress(e) {
+  if (done) {
+    return;
+  }
+
+  const action = e.key;
+  console.log(action);
+
+  if (action === "Enter") {
+    commit();
+  } else if (action === "Backspace") {
+    backspace();
+  } else if (isLetter(action)) {
+    addLetter(action.toUpperCase());
+  } else {
+    alert("Not a valid letter!");
+  }
+});
 
 // API CALL TO GET WORD OF THE DAY
 
-let getWord = async function (){
-    //define the URL we send a get request to "get" data from
-    URL = "https://words.dev-apis.com/word-of-the-day";
-    //declare variable, store our fetch function in it
-    const promise = await fetch(URL);
-    console.log(promise);
-    const processedResponse = await(promise.json());
-    let wordOfTheDay = processedResponse.word;
-    console.log(wordOfTheDay)
+let getWord = async function () {
+  //define the URL we send a get request to "get" data from
+  URL = "https://words.dev-apis.com/word-of-the-day";
+  //declare variable, store our fetch function in it
+  const promise = await fetch(URL);
+  console.log(promise);
+  const processedResponse = await promise.json();
+  let wordOfTheDay = processedResponse.word;
+  console.log(wordOfTheDay);
+};
 
-}
-
-//POST method to see if word is valid 
+//POST method to see if word is valid
 
 //handle a keystroke
 /*
@@ -107,8 +146,24 @@ let keyStroke = function(){
 
 //checking if key is letter
 
-function isLetter(letter){
-    return /^[a-zA-Z]$/.test(letter);
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
 }
 
-init()
+//makes an object of the letters of the array
+
+function makeMap(array) {
+  const obj = {};
+  for (let i = 0; i < array.length; i++) {
+    const letter = array[i];
+    if (obj[letter]) {
+      //returns true
+      obj[letter]++;
+    } else {
+      obj[letter] = 1;
+    }
+  }
+  return obj;
+}
+}
+init();
